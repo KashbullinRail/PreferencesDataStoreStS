fad8b6ee7a83357e7b61f5cf133cea481408c088
KashbullinRail <kashbullinrail@gmail.com>
2022-12-08 20:47:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/preferencesdatastorests/repository/DataStoreRepository.kt
@@ -0,0 +1,16 @@
+package com.example.preferencesdatastorests.repository
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.*
+import androidx.datastore.preferences.core.Preferences
+
+
+
+const val PREF_NAME = "Preferences"
+
+class DataStoreRepository(context: Context) {
+
+    private val dataStore: DataStore<Preferences> =
+
+}
